94f762db428b63f5ff6e0993cc1689a0be4db3bb
brown.towel <brown.towel40@gmail.com>
2023-02-05 11:17:00 +0900

    second commit

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -5,9 +5,51 @@ document.addEventListener("DOMContentLoaded", function() {
   liff
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
+
+        document.getElementById("liff").innerHTML = "1";
+
+        const contextToken = ((token) => {   
+            
+            document.getElementById("liff").innerHTML = document.getElementById("liff").innerHTML + "2"
+
+            const base64Url = token.split('.')[1];                             
+            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');    
+
+            return JSON.parse( decodeURIComponent( escape( window.atob(base64) ) ) );
+
+        })( liff.getDecodedIDToken().contextToken );
+
+        document.getElementById("liff").innerHTML = document.getElementById("liff").innerHTML + "3"
+
+        document.getElementById("liff").innerHTML = JSON.stringify(contextToken);
+
+        document.getElementById("liff").innerHTML = document.getElementById("liff").innerHTML + "4"
+
+        const url = process.env.LINE_LOGIN_API_ENDPOINT;
+        const option = {
+            method: "POST",
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: (( data ) => {
+                const form = new FormData();
+
+                Object
+                  .keys(data)
+                  .forEach(key => form.append(key, data[key]));
+
+                return form;
+
+            })( { id_token: id_token, client_id: process.env.CHANNEL_ID } )
+        };
+
+        fetch(url, option).then((data) => {
+            console.log(data);
+            document.getElementById("liff").innerHTML = JSON.stringify(data.json());
+        });
+
         console.log("Success! you can do something with LIFF API here.")
     })
     .catch((error) => {
+        document.getElementById("catch").innerHTML = error
         console.log(error)
     })
 });
